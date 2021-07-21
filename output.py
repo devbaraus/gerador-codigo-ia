35d273fb00ba9330a50752157d4dd820b273d421
Samanosukeh <flauberth_94@hotmail.com>
2021-07-21 14:32:19 -0300

    .

--- output.py
+++ output.py
@@ -2,22 +2,32 @@ import pandas as pd
 base1 = pd.read_csv('credit_data.csv')
 
 #Substituir a linha abaixo pela coluna de inicio dos atributos previsores
-inicio_previsores_base1 = 0
+inicio_previsores_base1 = 1
 #Substituir a linha abaixo pelo numero da coluna classe
-coluna_classe_base1 = None
+coluna_classe_base1 = 6
 
 previsores_base1 = base1.iloc[:, inicio_previsores_base1:coluna_classe_base1].values
 classe_base1 = base1.iloc[:, coluna_classe_base1].values
+
 
 base2 = pd.read_csv('risco_credito.csv')
 
 #Substituir a linha abaixo pela coluna de inicio dos atributos previsores
 inicio_previsores_base2 = 0
 #Substituir a linha abaixo pelo numero da coluna classe
-coluna_classe_base2 = None
+coluna_classe_base2 = 7
 
 previsores_base2 = base2.iloc[:, inicio_previsores_base2:coluna_classe_base2].values
 classe_base2 = base2.iloc[:, coluna_classe_base2].values
+
+
+#-------- Tratando os valores faltantes -----------#
+from sklearn.impute import SimpleImputer
+import numpy as np
+estrategia = "mean"
+imputer = SimpleImputer(missing_values = np.nan, strategy = estrategia)
+imputer = imputer.fit(previsores_base1[:, inicio_previsores_base1:coluna_classe_base1])
+previsores_base1[:, inicio_previsores_base1:coluna_classe_base1] = imputer.transform(previsores_base1[:, inicio_previsores_base1:coluna_classe_base1])
 
 
 #----------Escalonando os atributos -----------#
